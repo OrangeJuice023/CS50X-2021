087d21cd100818b24bdf27a1a4f372cbe11c4f66
Gervi Paulo C. Corado <87125976+OrangeJuice023@users.noreply.github.com>
2023-07-01 09:24:22 +0800

    Rename 7 (1).sql to 7.sql

--- Week 7/Movies/7 (1).sql
+++
@@ -1,1 +0,0 @@
-SELECT DISTINCT movies.title, ratings.rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE movies.year LIKE "%2010%" ORDER BY 2 DESC, movies.title ASC LIMIT 10000000000000000;
